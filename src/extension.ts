069737cca2a0b941820703b4e3d3af3165165585
zhousaixing <1649364691@qq.com>
2024-08-07 11:58:34 +0800

    choose webpack and test success!

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as grpc from 'grpc';
-import * as protoLoader from '@grpc/proto-loader';
-import * as path from 'path';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,39 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-plugin-grpc" is now active!');
 
-	// Load the protobuf
-	// 获取 greeter.proto 文件的相对路径
-	const protoPathRelative = path.join(__dirname, '..', 'proto', 'greeter.proto');
-	// 将相对路径转换为 Uri 文件路径，并获得文件系统路径
-    const protoPath = vscode.Uri.file(protoPathRelative).fsPath;
-    const packageDefinition = protoLoader.loadSync(protoPath, {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
-
-	const greeterProto = grpc.loadPackageDefinition(packageDefinition).Greeter;
-
-    // Define gRPC client
-    const client = new greeterProto('localhost:50051', grpc.credentials.createInsecure());
-
-    // Function to call gRPC method
-    function callGrpcMethod() {
-        const request = {
-            name: 'World'
-        };
-
-        client.sayHello(request, (error: any, response: any) => {
-            if (!error) {
-                vscode.window.showInformationMessage('Greeting: ' + response.message);
-            } else {
-                vscode.window.showErrorMessage('Error: ' + error.message);
-            }
-        });
-    }
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -53,8 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from vscode-plugin-grpc!');
-		// 调用grpc服务
-		callGrpcMethod();
 	});
 
 	context.subscriptions.push(disposable);
